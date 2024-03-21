f30699b0f6e6826768137fc974e94d65432d273c
Gabriel La Cruz <65361075+gflacruz@users.noreply.github.com>
2024-03-20 22:00:35 -0400

    Update mode-toggle.tsx

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
+
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
